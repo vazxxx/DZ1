3858c19fe846501bebea0b396d8122602a142196
VAZ <vazxxx@mail.ru>
2022-12-06 16:33:44 +0300

    добавил информацию

---
+++ 3адача 6/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine ("является ли число чётным");
+
+int number1 = int.Parse(Console.ReadLine()!);
+
+
+if (number1 % 2 ==0)
+{
+ Console.WriteLine(Чётное);
+}
+
+else
+
+{
+  Console.WriteLine(нечётное);
+}
---
+++ Задача 4/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine ("Введи 3 чесла");
+int number1 = int.Parse(Console.ReadLine()!);
+int number2 = int.Parse(Console.ReadLine()!);
+int number3 = int.Parse(Console.ReadLine()!);
+
+int max = number1;
+
+if (number2 > max)
+{
+  max = number2;
+}
+
+if (number3 > max)
+
+{
+  max = number3;
+}
+System.Console.WriteLine(max);
